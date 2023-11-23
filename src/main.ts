6d2b8f8bd21571b7c38142fa596a29c3b67bcff0
jixinbao <jixinbao@dcloud.io>
2023-11-23 15:56:06 +0800

    feat: 降级 pinia 为 2.0.x 可以正常使用

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,19 @@
 // import './assets/main.css'
 
 import { createSSRApp } from 'vue'
-// import { createPinia } from 'pinia'
+import { createPinia } from 'pinia'
 
 import App from './App.vue'
 // import router from './router'
 
 export function createApp() {
   const app = createSSRApp(App)
+  app.use(createPinia())
   return {
     app
   }
 }
 
-// app.use(createPinia())
 // app.use(router)
 
 // app.mount('#app')
